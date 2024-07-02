c63d58608a1303273acd26fea6b2371caf6c8d86
MaelStudio <66912947+MaelStudio@users.noreply.github.com>
2024-07-02 11:21:47 -0400

    Read IMU and barometer data

--- RocketComputer.ino
+++ RocketComputer.ino
@@ -1,36 +1,50 @@
 #include "globals.h"
+#include <Wire.h>
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_BMP280.h>
+#include <SD_MMC.h>
+
+Adafruit_MPU6050 mpu;
+Adafruit_BMP280 bmp;
+
+#define SEA_LEVEL_HPA 1021.50
 
 void setup() {
   Serial.begin(115200);
+
   pinMode(D0, INPUT_PULLUP);
 
   if (startStorage()) Serial.printf("SD card mounted. Size: %s\n", fmtSize(SD_MMC.cardSize()));
   else Serial.println("[!] SD card initialization failed");
 
+  if (!psramFound()) Serial.println("[!] PSRAM is disabled");
+
   if (startCam()) Serial.println("Camera initialized");
   else Serial.println("[!] Camera init failed");
 
-  prepRecording();
+  if (prepRecording()) Serial.println("Ready to record");
+  else Serial.println("[!] Failed to get camera frame");
+
+  if (mpu.begin()) Serial.println("MPU6050 initialized");
+  else Serial.println("[!] MPU6050 init failed");
+
+  if (bmp.begin(0x76)) Serial.println("BMP280 initialized");
+  else Serial.println("[!] BMP280 init failed");
+
+  mpu.setAccelerometerRange(MPU6050_RANGE_16_G);
+  mpu.setGyroRange(MPU6050_RANGE_250_DEG);
+  mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
   
   Serial.println("Setup complete");
 }
 
 void loop() {
-  while (!digitalRead(D0)) {
-    delay(10);
-  }
-  while (digitalRead(D0)) {
-    delay(10);
-  }
+  // get IMU data
+  sensors_event_t accel, gyro, t;
+  mpu.getEvent(&accel, &gyro, &t);
 
-  forceRecord = true;
-
-  while (!digitalRead(D0)) {
-    delay(10);
-  }
-  while (digitalRead(D0)) {
-    delay(10);
-  }
-
-  forceRecord = false;
+  // get barometer data
+  float pressure = bmp.readPressure();
+  float altitude = bmp.readAltitude(SEA_LEVEL_HPA);
+  float temp = bmp.readTemperature();
 }
