621cbadee142d25149f9c748e40dee27406f0f41
MaelStudio <66912947+MaelStudio@users.noreply.github.com>
2024-07-03 21:26:34 -0400

    BMP280 Sampling mode

--- RocketComputer.ino
+++ RocketComputer.ino
@@ -34,6 +34,12 @@ void setup() {
   mpu.setAccelerometerRange(MPU6050_RANGE_16_G);
   mpu.setGyroRange(MPU6050_RANGE_250_DEG);
   mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
+
+  bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     /* Operating Mode. */
+                  Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
+                  Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
+                  Adafruit_BMP280::FILTER_X16,      /* Filtering. */
+                  Adafruit_BMP280::STANDBY_MS_1);   /* Standby time. */
   
   Serial.println("Setup complete");
 
